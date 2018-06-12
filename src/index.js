0ed46e51695309b82037378c067cbb576be851d8
Chris <chen1385@gmail.com>
2018-06-12 15:00:34 -0700

    changed logo, moved homepage to components, changed styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// import {Provider} from 'react-redux';
+// import {createStore, applyMiddleware} from 'redux';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+// import promise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Homepage from './components/Homepage.jsx'
+// import reducers from './reducers';
+
+// const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Switch>
+                <Route path="/" component={Homepage} />
+        </Switch>
+        </div>
+    </BrowserRouter>
+, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,8 @@
+import {} from '../actions';
+import _ from 'lodash';
+
+export default function (state = {}, action) {
+    switch(action.type) {
+
+    }
+}
